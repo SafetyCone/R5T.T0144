1b1858b60159b543ac84d0fbf7e878728a98c7f6
davidcoats <david@getsafetycone.com>
2025-09-15 10:46:05 -0700

    Interim commit.

--- source/R5T.T0144/Code/_Types/_Classes/Authentication.cs
+++ source/R5T.T0144/Code/_Types/_Classes/Authentication.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 
+using F10Y.T0011;
+
 using R5T.T0142;
 
 
 namespace R5T.T0144
 {
     [DataTypeMarker]
+    [InstanceIdentity("1549F133-B8B0-4DE0-8077-BD52B62DEEE2")]
     public class Authentication : IAuthentication
     {
         public string Username { get; set; }
